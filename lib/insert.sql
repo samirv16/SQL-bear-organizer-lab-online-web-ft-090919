db31c618d8cb5bdd243f6ae3400027380dc13172
edras vallecillo <samir.vallecillo@outlook.com>
2019-10-14 22:15:20 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 17, "F", "black", "Sanguine", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 3, "F", "black", "Phlegmatic", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 7, "F", "white", "Choleric", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 10, "F", "brown", "Melancholic", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 20, "F", "brown", "Sanguine", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 8, "F", "green", "Choleri", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 11, "F", "white", "Sanguine", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (Null, 7, "F", "black", "Sanguine", 1);
